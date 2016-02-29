4d0132437f2e95a2fc61e496ea742e0042e42991
rkananennwg <rkananen@newgistics.com>
2016-02-28 22:03:17 -0600

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,80 +1,97 @@
 
 # load dplyr library if not available
 # the latest released version from CRAN with
-# install.packages("dplyr")
+#install.packages("knitr")
 library(data.table)
 library(dplyr)
+library(knitr)
 
 
 #set working directory to file location
 setwd("H:/datasciencecoursera/getdata-projectfiles-UCI HAR Dataset")
 
-featureNames <- read.table("UCI HAR Dataset/features.txt")
-activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
-subjecttrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
-activitytrain <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
-featurestrain <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
 
-subjecttest <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
-activitytest <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
-featurestest <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
 
-subject <- rbind(subjectTrain, subjectTest)
-activity <- rbind(activityTrain, activityTest)
-features <- rbind(featuresTrain, featuresTest)
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
 
-colnames(features) <- t(featureNames[2])
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
 
-colnames(activity) <- "Activity"
-colnames(subject) <- "Subject"
-completedata <- cbind(features,activity,subject)
+str(dataActivityTest)
+
+str(dataActivityTrain)
+
+str(dataSubjectTrain)
+
+str(dataSubjectTest)
+
+str(dataFeaturesTest)
+    
+str(dataFeaturesTrain)
+
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+str(Data)
+
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+
+head(dataActivity,30)
 
 
-columnsWithMeanSTD <- grep(".*Mean.*|.*Std.*", names(completeData), ignore.case=TRUE)
-
-requiredColumns <- c(columnsWithMeanSTD, 562, 563)
-dim(completeData)
-
-extractedData <- completeData[,requiredColumns]
-dim(extractedData)
-
-extractedData$Activity <- as.character(extractedData$Activity)
-for (i in 1:6){
-  extractedData$Activity[extractedData$Activity == i] <- as.character(activityLabels[i,2])
-}
-
-
-extractedData$Activity <- as.factor(extractedData$Activity)
-
-
-
-names(extractedData)
-
-
-# adding full names to give more context
-names(extractedData)<-gsub("Acc", "Accelerometer", names(extractedData))
-names(extractedData)<-gsub("Gyro", "Gyroscope", names(extractedData))
-names(extractedData)<-gsub("BodyBody", "Body", names(extractedData))
-names(extractedData)<-gsub("Mag", "Magnitude", names(extractedData))
-names(extractedData)<-gsub("^t", "Time", names(extractedData))
-names(extractedData)<-gsub("^f", "Frequency", names(extractedData))
-names(extractedData)<-gsub("tBody", "TimeBody", names(extractedData))
-names(extractedData)<-gsub("-mean()", "Mean", names(extractedData), ignore.case = TRUE)
-names(extractedData)<-gsub("-std()", "STD", names(extractedData), ignore.case = TRUE)
-names(extractedData)<-gsub("-freq()", "Frequency", names(extractedData), ignore.case = TRUE)
-names(extractedData)<-gsub("angle", "Angle", names(extractedData))
-names(extractedData)<-gsub("gravity", "Gravity", names(extractedData))
+# rename for better readability
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
 
 
 
-#set subject variable
-extractedData$Subject <- as.factor(extractedData$Subject)
-extractedData <- data.table(extractedData)
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
+library(knitr)
+knit2html("codebook.Rmd");
 
 
-tidydata <- aggregate(. ~Subject + Activity, extractedData, mean)
-tidydata <- tidydata[order(tidydata$Subject,tidydata$Activity),]
-write.table(tidydata, file = "Tidy.txt", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
